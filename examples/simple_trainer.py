cancel using log space of parameters. Jacobian has some coalesced values which may be a bug

examples/simple_trainer.py
@@ -377,9 +377,8 @@ class Runner:
         Ks: Tensor,
         width: int,
         height: int,
-        get_jacobian: bool = False,
         **kwargs,
-    ):
+    ) -> Tuple[Tensor, Tensor, Dict]:
         means = self.splats["means"]  # [N, 3]
         # quats = F.normalize(self.splats["quats"], dim=-1)  # [N, 4]
         # rasterization does normalization internally
@@ -388,7 +387,6 @@ class Runner:
         opacities = torch.sigmoid(self.splats["opacities"])  # [N,]
 
         image_ids = kwargs.pop("image_ids", None)
-        target_colors = kwargs.pop("target_colors", Tensor)
         if self.cfg.app_opt:
             colors = self.app_module(
                 features=self.splats["features"],
@@ -404,63 +402,28 @@ class Runner:
             colors = torch.cat([self.splats["sh0"], self.splats["shN"]], 1)  # [N, K, 3]
 
         rasterize_mode = "antialiased" if self.cfg.antialiased else "classic"
-        if get_jacobian:
-            ## jacobian
-            kwargs.pop("render_mode", str)
-            render_colors, render_alphas, info, jacobian = rasterization_jacobian(
-                means=means,
-                quats=quats,
-                scales=scales,
-                opacities=opacities,
-                coeffs=colors,
-                viewmats=torch.linalg.inv(camtoworlds),  # [C, 4, 4]
-                Ks=Ks,  # [C, 3, 3]
-                target_colors=target_colors,
-                rasterize_mode=rasterize_mode,
-                width=width,
-                height=height,
-                distributed=self.world_size > 1,
-                **kwargs,
-            )
-            return render_colors, render_alphas, info, jacobian
-        else:
-            ## CUDA complementation
-            render_colors, render_alphas, info = rasterization(
-                means=means,
-                quats=quats,
-                scales=scales,
-                opacities=opacities,
-                colors=colors,
-                viewmats=torch.linalg.inv(camtoworlds),  # [C, 4, 4]
-                Ks=Ks,  # [C, 3, 3]
-                width=width,
-                height=height,
-                packed=self.cfg.packed,
-                absgrad=(
-                    self.cfg.strategy.absgrad
-                    if isinstance(self.cfg.strategy, DefaultStrategy)
-                    else False
-                ),
-                sparse_grad=self.cfg.sparse_grad,
-                rasterize_mode=rasterize_mode,
-                distributed=self.world_size > 1,
-                **kwargs,
-            )
-            ## torch complementation
-            # render_colors, render_alphas, info = _rasterization(
-            #     means=means,
-            #     quats=quats,
-            #     scales=scales,
-            #     opacities=opacities,
-            #     colors=colors,
-            #     viewmats=torch.linalg.inv(camtoworlds),  # [C, 4, 4]
-            #     Ks=Ks,  # [C, 3, 3]
-            #     width=width,
-            #     height=height,
-            #     rasterize_mode=rasterize_mode,
-            #     **kwargs,
-            # )
-            return render_colors, render_alphas, info
+        render_colors, render_alphas, info = rasterization(
+            means=means,
+            quats=quats,
+            scales=scales,
+            opacities=opacities,
+            colors=colors,
+            viewmats=torch.linalg.inv(camtoworlds),  # [C, 4, 4]
+            Ks=Ks,  # [C, 3, 3]
+            width=width,
+            height=height,
+            packed=self.cfg.packed,
+            absgrad=(
+                self.cfg.strategy.absgrad
+                if isinstance(self.cfg.strategy, DefaultStrategy)
+                else False
+            ),
+            sparse_grad=self.cfg.sparse_grad,
+            rasterize_mode=rasterize_mode,
+            distributed=self.world_size > 1,
+            **kwargs,
+        )
+        return render_colors, render_alphas, info
 
     def train(self):
         cfg = self.cfg
@@ -539,19 +502,17 @@ class Runner:
             sh_degree_to_use = min(step // cfg.sh_degree_interval, cfg.sh_degree)
 
             # forward
-            renders, alphas, info, jacobians = self.rasterize_splats(
+            renders, alphas, info = self.rasterize_splats(
                 camtoworlds=camtoworlds,
                 Ks=Ks,
                 width=width,
                 height=height,
-                get_jacobian=True,
                 # kargs
                 sh_degree=sh_degree_to_use,
                 near_plane=cfg.near_plane,
                 far_plane=cfg.far_plane,
                 image_ids=image_ids,
                 render_mode="RGB+ED" if cfg.depth_loss else "RGB",
-                target_colors = pixels,
             )
             if renders.shape[-1] == 4:
                 colors, depths = renders[..., 0:3], renders[..., 3:4]
@@ -596,7 +557,6 @@ class Runner:
                 disp_gt = 1.0 / depths_gt  # [1, M]
                 depthloss = F.l1_loss(disp, disp_gt) * self.scene_scale
                 loss += depthloss * cfg.depth_lambda
-
             loss.backward()
 
             desc = f"loss={loss.item():.3f}| " f"sh degree={sh_degree_to_use}| "
@@ -607,6 +567,15 @@ class Runner:
                 pose_err = F.l1_loss(camtoworlds_gt, camtoworlds)
                 desc += f"pose err={pose_err.item():.6f}| "
             pbar.set_description(desc)
+
+            # write images (gt and render)
+            # if world_rank == 0 and step % 800 == 0:
+            #     canvas = torch.cat([pixels, colors], dim=2).detach().cpu().numpy()
+            #     canvas = canvas.reshape(-1, *canvas.shape[2:])
+            #     imageio.imwrite(
+            #         f"{self.render_dir}/train_rank{self.world_rank}.png",
+            #         (canvas * 255).astype(np.uint8),
+            #     )
 
             if world_rank == 0 and cfg.tb_every > 0 and step % cfg.tb_every == 0:
                 mem = torch.cuda.max_memory_allocated() / 1024**3

examples/trainer_jacobian.py
@@ -181,7 +181,7 @@ def create_splats_with_optimizers(
     dist2_avg = (knn(points, 4)[:, 1:] ** 2).mean(dim=-1)  # [N,]
     dist_avg = torch.sqrt(dist2_avg)
     # 基于近邻距离取对数值初始化，并复制成3个相同的值，后续再exp运算，但优化时是根据对数空间的参数进行优化
-    scales = torch.log(dist_avg * init_scale).unsqueeze(-1).repeat(1, 3)  # [N, 3]
+    scales = (dist_avg * init_scale).unsqueeze(-1).repeat(1, 3)  # [N, 3]
 
     # Distribute the GSs to different ranks (also works for single rank)
     points = points[world_rank::world_size]
@@ -193,7 +193,7 @@ def create_splats_with_optimizers(
     quats = torch.rand((N, 4))  # [N, 4]
 
     # 用logit函数初始化不透明度，后续再sigmoid运算，但优化时是根据对数空间的参数进行优化
-    opacities = torch.logit(torch.full((N,), init_opacity))  # [N,]
+    opacities = torch.full((N,), init_opacity)  # [N,]
 
     params = [
         # name, value, lr
@@ -379,13 +379,13 @@ class Runner:
         height: int,
         get_jacobian: bool = False,
         **kwargs,
-    ):
+    )-> Tuple[Tensor, Tensor, Dict,Optional[Tensor]]:
         means = self.splats["means"]  # [N, 3]
         # quats = F.normalize(self.splats["quats"], dim=-1)  # [N, 4]
         # rasterization does normalization internally
         quats = self.splats["quats"]  # [N, 4]
-        scales = torch.exp(self.splats["scales"])  # [N, 3]
-        opacities = torch.sigmoid(self.splats["opacities"])  # [N,]
+        scales = self.splats["scales"]  # [N, 3]
+        opacities = self.splats["opacities"]  # [N,]
 
         image_ids = kwargs.pop("image_ids", None)
         target_colors = kwargs.pop("target_colors", Tensor)
@@ -460,7 +460,7 @@ class Runner:
             #     rasterize_mode=rasterize_mode,
             #     **kwargs,
             # )
-            return render_colors, render_alphas, info
+            return render_colors, render_alphas, info, None
 
     def train(self):
         cfg = self.cfg
@@ -591,6 +591,15 @@ class Runner:
             residual = 0.5 * ((colors - pixels)**2).sum(dim=-1, keepdim=True)
             residual = residual.flatten()
 
+            ## ========================compare autograd and jacobian===================
+            row_indices = jacobian.indices()[0,:].unique()
+            for i in row_indices:
+                i = i.item()
+                residual[i].backward(retain_graph=True)
+                for optimizer in self.optimizers.values():
+                    optimizer.zero_grad(set_to_none=True)
+            ## ========================compare autograd and jacobian===================
+
             # loss
             l1loss = F.l1_loss(colors, pixels)
             ssimloss = 1.0 - self.ssim(
@@ -598,6 +607,25 @@ class Runner:
                 pixels.permute(0, 3, 1, 2), colors.permute(0, 3, 1, 2)
             )
             loss = l1loss * (1.0 - cfg.ssim_lambda) + ssimloss * cfg.ssim_lambda
+            if cfg.depth_loss:
+                # query depths from depth map
+                points = torch.stack(
+                    [
+                        points[:, :, 0] / (width - 1) * 2 - 1,
+                        points[:, :, 1] / (height - 1) * 2 - 1,
+                    ],
+                    dim=-1,
+                )  # normalize to [-1, 1]
+                grid = points.unsqueeze(2)  # [1, M, 1, 2]
+                depths = F.grid_sample(
+                    depths.permute(0, 3, 1, 2), grid, align_corners=True
+                )  # [1, 1, M, 1]
+                depths = depths.squeeze(3).squeeze(1)  # [1, M]
+                # calculate loss in disparity space
+                disp = torch.where(depths > 0.0, 1.0 / depths, torch.zeros_like(depths))
+                disp_gt = 1.0 / depths_gt  # [1, M]
+                depthloss = F.l1_loss(disp, disp_gt) * self.scene_scale
+                loss += depthloss * cfg.depth_lambda
             loss.backward()
 
             desc = f"loss={loss.item():.3f}| " f"sh degree={sh_degree_to_use}| "
@@ -725,7 +753,7 @@ class Runner:
 
             torch.cuda.synchronize()
             tic = time.time()
-            colors, _, _ = self.rasterize_splats(
+            colors, _, _, _ = self.rasterize_splats(
                 camtoworlds=camtoworlds,
                 Ks=Ks,
                 width=width,
@@ -801,7 +829,7 @@ class Runner:
 
         canvas_all = []
         for i in tqdm.trange(len(camtoworlds), desc="Rendering trajectory"):
-            renders, _, _ = self.rasterize_splats(
+            renders, _, _, _ = self.rasterize_splats(
                 camtoworlds=camtoworlds[i : i + 1],
                 Ks=K[None],
                 width=width,
@@ -842,7 +870,7 @@ class Runner:
         c2w = torch.from_numpy(c2w).float().to(self.device)
         K = torch.from_numpy(K).float().to(self.device)
 
-        render_colors, _, _ = self.rasterize_splats(
+        render_colors, _, _, _ = self.rasterize_splats(
             camtoworlds=c2w[None],
             Ks=K[None],
             width=W,

gsplat/LMoptimizer.py
@@ -139,9 +139,9 @@ class LevenbergMarquardt(Optimizer):
         col_range (tuple): 要切片的列范围，格式为 (col_start, col_end)。
 
         返回:
-        torch.Tensor: 切片后的稀疏张量，仍为 COO 格式。
+        torch.Tensor: 切片后的稀疏张量
         """
-
+        assert A.layout == torch.sparse_coo,A.layout
         # 提取 COO 张量的 indices 和 values
         indices = A._indices()
         values = A._values()

gsplat/rendering.py
@@ -761,7 +761,7 @@ def rasterization_jacobian(
             absgrad=False,
         )
     residual_render_colors = render_colors[..., :3] - target_colors
-    jacobians_camera_indices, jacobians_row_indices, jacobians_col_indices, jacobians_values_indices = jacobians_bwd(
+    jacobian_camera_indices, jacobian_row_indices, jacobian_col_indices, jacobian_values = jacobians_bwd(
         means=means,
         covars=None,
         quats=quats,
@@ -785,10 +785,36 @@ def rasterization_jacobian(
     )
     if (C != 1):
         raise NotImplementedError("Only support single camera rendering.")
-    coo_indices = torch.stack(
-        [jacobians_row_indices, jacobians_col_indices], dim=0
-    )
-    jacobian = torch.sparse_coo_tensor(coo_indices, jacobians_values_indices, (width * height, parameters_per_gaussian * N))
+
+    ## =====================================draw the distribution==================
+    # abs_values = jacobian_values.abs()
+    # # 使用 log10 计算每个值的数量级
+    # log_values = torch.log10(abs_values)
+    # # 使用 floor 计算每个值的下边界
+    # floor_values = torch.floor(log_values)
+    # # 统计每个数量级的频率
+    # # 将数量级转换为整数索引
+    # min_floor_value = int(floor_values.min().item())
+    # bins = (floor_values - min_floor_value).to(torch.int64)
+    # counts = torch.bincount(bins)
+    # # 转换为 numpy 进行处理
+    # import numpy
+    # import matplotlib.pyplot as plt
+    # bin_edges = numpy.arange(min_floor_value, min_floor_value + len(counts) + 1)
+    # counts = counts.cpu().numpy()
+    # # 绘制直方图
+    # plt.figure(figsize=(10, 6))
+    # plt.hist(bin_edges[:-1], bins=bin_edges, weights=counts, edgecolor='black', log=True)
+    # plt.xlabel('Log10 Scale (x)')
+    # plt.ylabel('Frequency')
+    # plt.title('Distribution of Jacobian Values by Order of Magnitude')
+    # plt.xticks(bin_edges)
+    # plt.show()
+    ## =====================================draw the distribution==================
+
+    coo_indices = torch.stack([jacobian_row_indices, jacobian_col_indices], dim=0)
+    jacobian = torch.sparse_coo_tensor(coo_indices, jacobian_values, (width * height, parameters_per_gaussian * N))
+    jacobian = jacobian.coalesce()
     return render_colors, render_alphas, meta, jacobian
 
 def _rasterization(